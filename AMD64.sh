eda64477b39298a79484cca90574de197e23321f
23xvx <noxic34@gmail.com>
2023-04-30 09:55:47 +0800

    Update AMD64.sh

--- AMD64.sh
+++ AMD64.sh
@@ -1,47 +1,110 @@
 #!/data/data/com.termux/files/usr/bin/bash
-pkg install proot -y
-folder=ubuntu-fs64
-if [ -d "$folder" ]; then
-	first=1
-	echo "skipping downloading"
-fi
-tarball="ubuntu-rootfs.tar.xz"
-if [ "$first" != 1 ];then
-	if [ ! -f $tarball ]; then
-		echo "Download Rootfs, this may take a while base on your internet speed."
-		case `dpkg --print-architecture` in
-		aarch64)
-			archurl="amd64";
+
+ARCHITECTURE=$(dpkg --print-architecture)
+
+#clear
+clear 
+
+#Adding colors
+R="$(printf '\033[1;31m')"
+G="$(printf '\033[1;32m')"
+Y="$(printf '\033[1;33m')"
+W="$(printf '\033[1;37m')"
+C="$(printf '\033[1;36m')"
+
+#Warning
+echo ${R}"Warning!
+Error may occur during installation.
+Also, the distro speed will be more  lower than proot as it is in QEMU."
+sleep 3
+clear
+
+#requirements
+echo ${G}"Installing requirements"
+pkg install proot wget qemu-system-x86_64 -y
+clear
+termux-setup-storage
+clear
+
+#Notice
+echo ${C}"Make sure you download the rootfs for amd64/x86_64."
+echo ${C}"Your architecture is $ARCHITECTURE ."
+case `dpkg --print-architecture` in
+    aarch64)
+            echo ${G}"Please download the rootfs file for amd64." ;
+			sleep 1
+			echo ${G}"Downloading requirements for QEMU...."${W} ;
 			wget https://github.com/AllPlatform/Termux-UbuntuX86_64/raw/master/arm64/qemu-x86_64-static;
 			chmod 777 qemu-x86_64-static;
 			mv qemu-x86_64-static ~/../usr/bin ;;
-		arm)
-			archurl="amd64";
+    arm*)
+            echo ${G}"Please download the rootfs file for amd64." ;
+			sleep 1
+			echo ${G}"Downloading requirements for QEMU...."${W} ;
 			wget https://github.com/AllPlatform/Termux-UbuntuX86_64/raw/master/arm/qemu-x86_64-static;
 			chmod 777 qemu-x86_64-static;
 			mv qemu-x86_64-static ~/../usr/bin/ ;;
-		amd64)
-			archurl="amd64" ;;
-		x86_64)
-			archurl="amd64" ;;	
-		i*86)
-			archurl="i386" ;;
-		x86)
-			archurl="i386" ;;
-		*)
-			echo "unknown architecture"; exit 1 ;;
-		esac
-		wget "https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Rootfs/Ubuntu/${archurl}/ubuntu-rootfs-${archurl}.tar.xz" -O $tarball
-	fi
-	cur=`pwd`
-	mkdir -p "$folder"
-	cd "$folder"
-	echo "Decompressing Rootfs, please be patient."
-	proot --link2symlink tar -xJf ${cur}/${tarball}||:
-	cd "$cur"
+    i*86)
+            echo ${G}"Please download the rootfs file for x86."${W};
+			sleep 1 ;;
+	x86)	
+			echo ${G}"Please download the rootfs file for x86."${w};
+			sleep 1 ;;
+    x86_64)
+            echo ${G}"Please download the rootfs file for amd64."${W};
+			sleep 1 ;;
+    *)
+            echo ${R}"Unknown architecture"${W}; exit 1 ;;
+esac
+#Links
+echo ${G}"Please put in your URL here for downloading rootfs: "${W}
+read URL
+sleep 1
+echo ${G}"Please put in your distro name in order to login after all
+If you put in 'kali' , everytime you login will be 
+'bash kali-x64.sh' "${W}
+read ds_name
+sleep 1
+echo ${Y}"your URl is $URL 
+and your distro name is $ds_name "${W}
+sleep 2
+
+folder=$ds_name-x64-fs
+if [ -d "$folder" ]; then
+        echo ${G}"Existing file found, are you sure to remove it? (y or n)"${W}
+        read ans
+        if [[ "$ans" =~ ^([yY])$ ]]; then
+                echo ${W}"Deleting existing directory...."${W}
+                rm -rf ~/$folder
+                rm -rf ~/$ds_name-x64.sh
+                sleep 2
+        elif [[ "$ans" =~ ^([nN])$ ]]; then
+        echo ${R}"Sorry, but we cannot complete the installation"
+        exit
+        else 
+        echo
+        fi
+else 
+mkdir -p $folder
 fi
-mkdir -p ubuntu-binds
-bin=start-ubuntu64.sh
+
+#Downloading and decompressing rootfs
+clear
+echo ${G}"Downloading Rootfs....."${W}
+wget $URL -P ~/$folder/ 
+echo ${G}"Decompressing Rootfs....."
+proot --link2symlink \
+    tar -xpf ~/$folder/*.tar.* -C ~/$folder/ --exclude='dev'||:
+if [[ ! -d "$folder/etc" ]]; then
+     mv $folder/*/* $folder/
+fi
+echo "127.0.0.1 localhost" > ~/$folder/etc/hosts
+rm -rf ~/$folder/etc/resolv.conf
+echo "nameserver 8.8.8.8" > ~/$folder/etc/resolv.conf
+echo -e "#!/bin/sh\nexit" > "$folder/usr/bin/groups"
+mkdir -p $folder/binds
+
+bin=$ds_name-x64.sh
 echo "writing launch script"
 cat > $bin <<- EOM
 #!/bin/bash
@@ -54,7 +117,7 @@ command+=" -0"
 command+=" -r $folder -q qemu-x86_64-static"
 command+=" -b /dev"
 command+=" -b /proc"
-command+=" -b ubuntu-fs64/root:/dev/shm"
+command+=" -b $folder/root:/dev/shm"
 ## uncomment the following line to have access to the home directory of termux
 #command+=" -b /data/data/com.termux/files/home:/root"
 ## uncomment the following line to mount /sdcard directly to / 
@@ -79,5 +142,5 @@ termux-fix-shebang $bin
 echo "making $bin executable"
 chmod +x $bin
 echo "removing image for some space"
-rm $tarball
-echo "You can now launch Debian with the ./${bin} script"
+rm -rf $folder/*.tar.*
+echo "You can now launch your distro with the 'bash $bin' script"
